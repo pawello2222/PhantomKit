c9b5a4075ff6d6121af9064c745cf9c74ed6834c
pawello2222 <pawello2222@gmail.com>
2021-05-02 16:09:07 +0200

    Updated files

--- Sources/Resources/Localization/Helpers/LocalizableStrings.swift
+++ Sources/Resources/Localization/Helpers/LocalizableStrings.swift
@@ -10,7 +10,7 @@ import Foundation
 // swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
 internal enum Localized {
-  /// DoneX
+  /// Done
   internal static let done = Localized.tr("Localizable", "done")
 
   internal enum AboutView {
