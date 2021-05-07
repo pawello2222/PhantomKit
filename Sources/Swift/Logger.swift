fd60a0b87cab802c910a5363fd4fa04da2f2e19c
pawello2222 <pawello2222@gmail.com>
2021-05-07 22:11:00 +0200

    Added logger

---
+++ Sources/Swift/Logger.swift
@@ -0,0 +1,32 @@
+//
+//  Logger.swift
+//  PhantomKit
+//
+//  Created by Pawel Wiszenko on 07.05.2021.
+//  Copyright © 2021 Pawel Wiszenko. All rights reserved.
+//
+
+import Foundation
+import os.log
+
+public enum Logger {
+    public static func log(_ message: String, category: OSLog, type: OSLogType) {
+        os_log("%@", log: category, type: type, message)
+    }
+
+    public static func debug(_ message: String, category: OSLog) {
+        log(message, category: category, type: .debug)
+    }
+
+    public static func info(_ message: String, category: OSLog) {
+        log(message, category: category, type: .info)
+    }
+
+    public static func error(_ message: String, category: OSLog) {
+        log(message, category: category, type: .error)
+    }
+
+    public static func fault(_ message: String, category: OSLog) {
+        log(message, category: category, type: .fault)
+    }
+}
