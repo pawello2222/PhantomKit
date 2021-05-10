cc1865395e747027706cf536cd7028b92c481eb2
pawello2222 <pawello2222@gmail.com>
2021-05-11 00:22:56 +0200

    Added String Ext

---
+++ Sources/Swift/Extensions/String+Ext.swift
@@ -0,0 +1,15 @@
+//
+//  String+Ext.swift
+//  PhantomKit
+//
+//  Created by Pawel Wiszenko on 11.05.2021.
+//  Copyright © 2021 Pawel Wiszenko. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    public func withParentheses() -> String {
+        "(\(self)"
+    }
+}
