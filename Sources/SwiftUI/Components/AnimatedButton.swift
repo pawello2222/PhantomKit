baa770a56920bfb19d4d4301ef9fbfcdfa0e7386
pawello2222 <pawello2222@gmail.com>
2021-05-18 13:45:22 +0200

    Added Animated button

---
+++ Sources/SwiftUI/Components/AnimatedButton.swift
@@ -0,0 +1,39 @@
+//
+//  AnimatedButton.swift
+//  PhantomKit
+//
+//  Created by Pawel Wiszenko on 18.05.2021.
+//  Copyright © 2021 Pawel Wiszenko. All rights reserved.
+//
+
+import SwiftUI
+
+public struct AnimatedButton<Label>: View where Label: View {
+    private let action: () -> Void
+    private let label: () -> Label
+
+    public init(action: @escaping () -> Void, @ViewBuilder label: @escaping () -> Label) {
+        self.action = action
+        self.label = label
+    }
+
+    public var body: some View {
+        Button {
+            withAnimation {
+                action()
+            }
+        } label: {
+            label()
+        }
+    }
+}
+
+// MARK: - Convenience
+
+extension AnimatedButton where Label == Text {
+    public init(title: String, action: @escaping () -> Void) {
+        self.init(action: action) {
+            Text(title)
+        }
+    }
+}
