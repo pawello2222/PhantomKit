648ffdc2e157dd1b0fd054f40f1de9cc6e7391dd
pawello2222 <pawello2222@gmail.com>
2021-05-20 20:57:41 +0200

    Added GeometrySize EnvironmentKey

--- Sources/SwiftUI/Extensions/EnvironmentValues+Ext.swift
+++ Sources/SwiftUI/Extensions/EnvironmentValues+Ext.swift
@@ -21,6 +21,19 @@ extension EnvironmentValues {
     }
 }
 
+// MARK: - GeometrySize
+
+extension EnvironmentValues {
+    private struct GeometrySizeKey: EnvironmentKey {
+        static let defaultValue: CGSize = .zero
+    }
+
+    public var geometrySize: CGSize {
+        get { self[GeometrySizeKey] }
+        set { self[GeometrySizeKey] = newValue }
+    }
+}
+
 // MARK: - TabSelection
 
 extension EnvironmentValues {
@@ -39,6 +52,10 @@ extension EnvironmentValues {
 extension View {
     public func defaultIconHeight(_ value: CGFloat) -> some View {
         environment(\.defaultIconHeight, value)
+    }
+
+    public func geometrySize(_ size: CGSize) -> some View {
+        environment(\.geometrySize, size)
     }
 
     public func tabSelection<Selection>(_ selection: Selection) -> some View where Selection: Hashable {
