a2cab2afcb614db55c831cd5ddad0be771a463e7
pawello2222 <pawello2222@gmail.com>
2021-10-02 23:25:04 +0200

    Updated package

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/duemunk/Async", .upToNextMajor(from: "2.1.0")),
         .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "9.0.0")),
-        .package(name: "Xcore", url: "https://github.com/pawello2222/xcore.git", .upToNextMajor(from: "1.0.0")),
+        .package(name: "Xcore", url: "https://github.com/pawello2222/xcore.git", .branch("main")),
     ],
     targets: [
         .target(name: "PhantomKit", dependencies: ["Async", "Xcore"]),
