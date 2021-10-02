e4545d955cab87f988bab1672aec8cb4e99a6b36
pawello2222 <pawello2222@gmail.com>
2021-10-02 22:42:04 +0200

    Updated package

--- Package.swift
+++ Package.swift
@@ -2,23 +2,6 @@
 
 import PackageDescription
 
-// MARK: - Dependencies
-
-let depencencies: [Package.Dependency] = [
-    .package(url: "https://github.com/duemunk/Async", .upToNextMajor(from: "2.1.0")),
-    .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "9.0.0")),
-    .package(name: "Xcore", url: "https://github.com/pawello2222/xcore.git", .upToNextMajor(from: "1.0.0")),
-]
-
-// MARK: - Targets
-
-let targets: [Target] = [
-    .target(name: "PhantomKit", dependencies: ["Async", "Xcore"], path: "Sources"),
-    .testTarget(name: "PhantomKitTests", dependencies: ["PhantomKit", "Nimble"], path: "Tests"),
-]
-
-// MARK: - Package
-
 let package = Package(
     name: "PhantomKit",
     defaultLocalization: "en",
@@ -26,7 +9,14 @@ let package = Package(
     products: [
         .library(name: "PhantomKit", targets: ["PhantomKit"]),
     ],
-    dependencies: depencencies,
-    targets: targets,
+    dependencies: [
+        .package(url: "https://github.com/duemunk/Async", .upToNextMajor(from: "2.1.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "9.0.0")),
+        .package(name: "Xcore", url: "https://github.com/pawello2222/xcore.git", .upToNextMajor(from: "1.0.0")),
+    ],
+    targets: [
+        .target(name: "PhantomKit", dependencies: ["Async", "Xcore"], path: "Sources"),
+        .testTarget(name: "PhantomKitTests", dependencies: ["PhantomKit", "Nimble"], path: "Tests"),
+    ],
     swiftLanguageVersions: [.v5]
 )
