206fd81f86634ab8785b36f4d0cda66959871d12
pawello2222 <pawello2222@gmail.com>
2021-10-18 12:49:47 +0200

    Updated AnimatedButton

--- Sources/PhantomKit/SwiftUI/Components/Button/AnimatedButton.swift
+++ Sources/PhantomKit/SwiftUI/Components/Button/AnimatedButton.swift
@@ -9,16 +9,18 @@
 import SwiftUI
 
 public struct AnimatedButton<Label>: View where Label: View {
+    private let role: ButtonRole
     private let action: () -> Void
     private let label: () -> Label
 
-    public init(action: @escaping () -> Void, @ViewBuilder label: @escaping () -> Label) {
+    public init(role: ButtonRole, action: @escaping () -> Void, @ViewBuilder label: @escaping () -> Label) {
+        self.role = role
         self.action = action
         self.label = label
     }
 
     public var body: some View {
-        Button {
+        Button(role: role) {
             withAnimation {
                 action()
             }
@@ -31,8 +33,8 @@ public struct AnimatedButton<Label>: View where Label: View {
 // MARK: - Convenience
 
 extension AnimatedButton where Label == Text {
-    public init(_ title: String, action: @escaping () -> Void) {
-        self.init(action: action) {
+    public init(_ title: String, role: ButtonRole, action: @escaping () -> Void) {
+        self.init(role: role, action: action) {
             Text(title)
         }
     }
