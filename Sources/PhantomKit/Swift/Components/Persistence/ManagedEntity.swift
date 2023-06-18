8b0357fe910de62e6066fd1ae254e7fa6d955305
Pawel Wiszenko <16039496+pawello2222@users.noreply.github.com>
2023-06-19 00:57:52 +0200

    Refactoring

---
+++ Sources/PhantomKit/Swift/Components/Persistence/ManagedEntity.swift
@@ -0,0 +1,35 @@
+//
+//  ManagedEntity.swift
+//  PhantomKit
+//
+//  Created by Pawel Wiszenko on 11.06.2021.
+//  Copyright © 2021 Pawel Wiszenko. All rights reserved.
+//
+
+import CoreData
+
+public protocol ManagedEntity: NSFetchRequestResult {}
+
+extension ManagedEntity where Self: NSManagedObject {
+    public static var entityName: String {
+        let nameMO = String(describing: Self.self)
+        return nameMO.replacingOccurrences(of: "MO", with: "")
+    }
+
+    public static func insertNew(in context: NSManagedObjectContext) -> Self? {
+        NSEntityDescription
+            .insertNewObject(forEntityName: entityName, into: context) as? Self
+    }
+
+    public static func newFetchRequest(predicate: NSPredicate? = nil) -> NSFetchRequest<Self> {
+        let fetchRequest = NSFetchRequest<Self>(entityName: entityName)
+        fetchRequest.predicate = predicate
+        return fetchRequest
+    }
+
+    public static func newBatchDeleteRequest(predicate: NSPredicate? = nil) -> NSBatchDeleteRequest {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
+        fetchRequest.predicate = predicate
+        return .init(fetchRequest: fetchRequest)
+    }
+}
--- Sources/PhantomKit/SwiftUI/Extensions/View+Extensions.swift
+++ Sources/PhantomKit/SwiftUI/Extensions/View+Extensions.swift
@@ -70,6 +70,33 @@ extension View {
     }
 }
 
+// MARK: - Background
+
+extension View {
+    public func expandingBackground<V>(
+        _ background: @autoclosure @escaping () -> V,
+        ignoresSafeAreaEdges edges: Edge.Set = .all
+    ) -> some View where V: View {
+        expandingBackground(
+            ignoresSafeAreaEdges: edges,
+            background: background
+        )
+    }
+}
+
+extension View {
+    public func expandingBackground<V>(
+        ignoresSafeAreaEdges edges: Edge.Set = .all,
+        @ViewBuilder background: @escaping () -> V
+    ) -> some View where V: View {
+        ZStack {
+            background()
+                .ignoresSafeArea(.all, edges: edges)
+            self
+        }
+    }
+}
+
 // MARK: - FixedSize
 
 extension View {
